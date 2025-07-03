b916fb7e7cf4209c97301eaeb597f20b432230d8
adnanmahmud0 <adnan99mahmud@gmail.com>
2025-07-04 03:02:11 +0600

    bookApi created

---
+++ src/redux/services/bookApi.ts
@@ -0,0 +1,59 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+
+export const bookApi = createApi({
+  reducerPath: 'bookApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://library-management-five-delta.vercel.app/api/' }),
+  tagTypes: ['Books'],
+  endpoints: (builder) => ({
+    
+    //getbook
+    getBooks: builder.query({
+      query: () => 'books',
+      providesTags: ['Books'],
+    }),
+
+    //getbook by id
+    getBookById: builder.query({
+      query: (id) => `books/${id}`,
+    }),
+
+    //add book
+    addBook: builder.mutation({
+      query: (data) => ({
+        url: 'books',
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['Books'],
+    }),
+
+    //update book
+    updateBook: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `books/${id}`,
+        method: 'PUT',
+        body: data,
+      }),
+      invalidatesTags: ['Books'],
+    }),
+
+    //delete book
+    deleteBook: builder.mutation({
+      query: (id) => ({
+        url: `books/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Books'],
+    }),
+
+  }),
+});
+
+export const {
+  useGetBooksQuery,
+  useGetBookByIdQuery,
+  useAddBookMutation,
+  useUpdateBookMutation,
+  useDeleteBookMutation,
+} = bookApi;
