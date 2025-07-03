6aade6c4589deaec2e6eddf78aaa0d66a66e5543
adnanmahmud0 <adnan99mahmud@gmail.com>
2025-07-04 02:33:35 +0600

    book type declieard

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+// Book Type
+export interface IBook {
+  _id: string;
+  title: string;
+  author: string;
+  genre: string;
+  isbn: string;
+  description: string;
+  copies: number;
+  available: boolean;
+  createdAt?: string;
+  updatedAt?: string;
+}
