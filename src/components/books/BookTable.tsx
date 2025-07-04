2c9332c48edfac82bcc993b01e4e057ccc7771f4
adnanmahmud0 <adnan99mahmud@gmail.com>
2025-07-04 09:25:14 +0600

    book table unused import remove

--- src/components/books/BookTable.tsx
+++ src/components/books/BookTable.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import {
-    Table, TableBody, TableCaption, TableCell,
+    Table, TableBody, TableCell,
     TableHead, TableHeader, TableRow,
 } from "@/components/ui/table";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
