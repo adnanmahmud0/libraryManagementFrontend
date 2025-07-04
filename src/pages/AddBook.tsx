f4d54c06a984553e7e3f57691f9d3a7f206814a6
adnanmahmud0 <adnan99mahmud@gmail.com>
2025-07-04 08:16:33 +0600

    book add compunent are added

--- src/pages/AddBook.tsx
+++ src/pages/AddBook.tsx
@@ -3,18 +3,18 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 
 const AddBook = () => {
-  return (
-    <div className="flex justify-center mt-10 px-4">
-      <Card className="w-full max-w-xl">
-        <CardHeader>
-          <CardTitle>Add New Book</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <AddBookForm />
-        </CardContent>
-      </Card>
-    </div>
-  );
+    return (
+        <div className="flex justify-center mt-10 px-4">
+            <Card className="w-full max-w-xl">
+                <CardHeader>
+                    <CardTitle>Add New Book</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <AddBookForm />
+                </CardContent>
+            </Card>
+        </div>
+    );
 };
 
 export default AddBook;
