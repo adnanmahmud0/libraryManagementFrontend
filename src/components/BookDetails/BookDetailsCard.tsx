74bf1afe7c3b6ba3080e0c4ec2b77e95732d04a8
adnanmahmud0 <adnan99mahmud@gmail.com>
2025-07-05 14:25:10 +0600

    book detels compunent seperatipon is done

---
+++ src/components/BookDetails/BookDetailsCard.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+
+interface Props {
+    book: any;
+}
+
+const BookDetailsCard = ({ book }: Props) => {
+    return (
+        <Card className="border shadow-md mt-6">
+            <CardHeader>
+                <CardTitle className="text-2xl">{book?.title}</CardTitle>
+                <CardDescription>Details about the book</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-4">
+                <div>
+                    <p>Author:</p>
+                    <p className="text-base">{book?.author}</p>
+                </div>
+                <div>
+                    <p>Genre:</p>
+                    <p className="text-base">{book?.genre}</p>
+                </div>
+                <div>
+                    <p>ISBN:</p>
+                    <p className="text-base">{book?.isbn}</p>
+                </div>
+                <div>
+                    <p>Copies:</p>
+                    <p className="text-base">{book?.copies}</p>
+                </div>
+                <div>
+                    <p>Availability:</p>
+                    <p
+                        className={`text-base ${book?.available ? "text-green-600" : "text-red-600"}`}
+                    >
+                        {book?.available ? "Available" : "Unavailable"}
+                    </p>
+                </div>
+                <div>
+                    <p>Description:</p>
+                    <p className="text-base">{book?.description}</p>
+                </div>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default BookDetailsCard;
--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -1,17 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// src/pages/BookDetails.tsx
-
 import Title from "@/components/header/Title";
 import { useGetBookByIdQuery } from "@/redux/services/bookApi";
 import { useParams } from "react-router";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card";
-import { Skeleton } from "@/components/ui/skeleton"
+import { Skeleton } from "@/components/ui/skeleton";
+import BookDetailsCard from "@/components/BookDetails/BookDetailsCard";
+
 
 const BookDetails = () => {
     const { id } = useParams<{ id: string }>();
@@ -45,43 +38,7 @@ const BookDetails = () => {
     return (
         <div className="max-w-3xl mx-auto px-4 py-10">
             <Title title="Book Details" />
-            <Card className="border shadow-md mt-6">
-                <CardHeader>
-                    <CardTitle className="text-2xl">{book?.title}</CardTitle>
-                    <CardDescription>Details about the book</CardDescription>
-                </CardHeader>
-                <CardContent className="space-y-4">
-                    <div>
-                        <p>Author:</p>
-                        <p className="text-base">{book?.author}</p>
-                    </div>
-                    <div>
-                        <p>Genre:</p>
-                        <p className="text-base">{book?.genre}</p>
-                    </div>
-                    <div>
-                        <p>ISBN:</p>
-                        <p className="text-base">{book?.isbn}</p>
-                    </div>
-                    <div>
-                        <p>Copies:</p>
-                        <p className="text-base">{book?.copies}</p>
-                    </div>
-                    <div>
-                        <p>Availability:</p>
-                        <p
-                            className={`text-base${book?.available ? "text-green-600" : "text-red-600"
-                                }`}
-                        >
-                            {book?.available ? "Available" : "Unavailable"}
-                        </p>
-                    </div>
-                    <div>
-                        <p >Description:</p>
-                        <p className="text-base">{book?.description}</p>
-                    </div>
-                </CardContent>
-            </Card>
+            <BookDetailsCard book={book} />
         </div>
     );
 };
